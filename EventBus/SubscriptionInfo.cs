12ffa0b99bdc7789cae10fe69e31ed062975f8d9
Kim-bk <nguyenhoangkim120201@gmail.com>
2022-09-15 11:03:45 +0700

    set up EventBus RabbitMQ

---
+++ EventBus/SubscriptionInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EventBus
+{
+    public class SubscriptionInfo
+    {
+        public Type HandlerType { get; }
+
+        private SubscriptionInfo(Type handlerType)
+        {
+            HandlerType = handlerType;
+        }
+
+        public static SubscriptionInfo Typed(Type handlerType) =>
+            new SubscriptionInfo(handlerType);
+    }
+}

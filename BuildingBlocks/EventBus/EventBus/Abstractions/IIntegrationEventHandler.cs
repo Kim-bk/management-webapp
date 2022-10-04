364bddad52bcbaaa1fd09d1cea0cab266e0080c5
Kim-bk <nguyenhoangkim120201@gmail.com>
2022-10-04 11:09:18 +0700

    init BuildingBlocks

---
+++ BuildingBlocks/EventBus/EventBus/Abstractions/IIntegrationEventHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using EventBus.Events;
+
+namespace EventBus.Abstractions
+{
+    public interface IIntegrationEventHandler<in TIntegrationEvent> : IIntegrationEventHandler
+    where TIntegrationEvent : IntegrationEvent
+    {
+        Task Handle(TIntegrationEvent @event);
+    }
+
+    public interface IIntegrationEventHandler
+    {
+    }
+}
